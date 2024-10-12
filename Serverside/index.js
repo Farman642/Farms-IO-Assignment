7a73e3cda03777227b730a1638eb1caed8ef47ad
Farman <Farmankhan.7874@Gmail.com>
2024-10-12 19:52:39 +0530

    adding tasks

--- Serverside/index.js
+++ Serverside/index.js
@@ -1,18 +1,24 @@
 require("dotenv").config()
 const express = require("express");
 const bodyParser = require('body-parser');
+const cors = require('cors');
 
 const connectDB = require('./Databaseconnecting/db');
 const userRoutes = require('./routes/userRoutes');
+const taskRoutes = require('./routes/taskRoutes');
 
 const app = express();
 
 const PORT =  process.env.port || 5000;
 
-
+app.use(cors());
 connectDB()
+
 app.use(bodyParser.json());
 app.use('/users', userRoutes);
+app.use('/tasks', taskRoutes);
+
+
 app.get('/',(req,res)=>{
     res.send("Server is running");
 });
